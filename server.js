f6717099b76d3e16a5b266ca34c52e9ec6a8f88e
makakoa <makakoa15@gmail.com>
2014-10-30 12:24:11 -0700

    initial commit

--- server.js
+++ server.js
@@ -1,2 +1,28 @@
 var express = require('express');
-//your code here
+var app = express();
+
+app.get('/time', function(req,res) {
+	var d = new Date();
+	var h = d.getHours();
+	var m = d.getMinutes();
+	if(m<10) {
+		m = "0" + m;
+	}
+	var data = {
+		hour: h,
+		minute: m
+	};
+	res.json(data);
+});
+
+app.get('/:name', function(req,res) {
+	var data = {
+		msg: 'Hello ' + req.params.name + "!"
+	};
+	res.json(data);
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+	//console.log('server running on port ' + port);
+});
--- test/api_test.js
+++ test/api_test.js
@@ -1,8 +1,30 @@
 var chai = require('chai');
 var chaihttp = require('chai-http');
 chai.use(chaihttp);
+var server = require('../server.js')
 
 describe('Simple JSON API', function() {
-  it('should send the local time');
-  it('should greet someone');
+	it('should send the local time', function() {
+		var d = new Date();
+		var h = d.getHours();
+		var m = d.getMinutes();
+		if(m<10) {
+			m = "0" + m;
+		}
+	  	chai.request('http://localhost:3000')
+	  		.get('/time')
+	  		.end(function(err, res) {
+	  			chai.expect(err).to.be.null;
+	  			chai.expect(res.body.hour).to.eql(h);
+	  			chai.expect(res.body.minute).to.eql(m);
+	  		});
+	});
+	it('should greet someone', function() {
+  	chai.request('http://localhost:3000')
+  		.get('/name')
+  		.end(function(err, res) {
+  			chai.expect(err).to.be.null;
+  			chai.expect(res.body.msg).to.equal("Hello name!");
+  		});
+	});
 });
